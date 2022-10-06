ccd8758f231872a5d7bd86031e448608b791f83f
Dennis <dvdstelt@gmail.com>
2022-10-07 00:01:47 +0200

    Platform Sample added

--- src/Shared/Configuration/EndpointConfigurationExtensions.cs
+++ src/Shared/Configuration/EndpointConfigurationExtensions.cs
@@ -5,18 +5,20 @@ namespace Shared.Configuration
 {
     public static class EndpointConfigurationExtensions
     {
-        public static EndpointConfiguration ApplyCommonConfiguration(this EndpointConfiguration endpointConfiguration, Action<RoutingSettings> configureRouting = null)
+        public static EndpointConfiguration ApplyCommonConfiguration(this EndpointConfiguration endpointConfiguration,
+            Action<RoutingSettings> configureRouting = null)
         {
             var transport = endpointConfiguration.UseTransport<LearningTransport>();
             var routing = transport.Routing();
             configureRouting?.Invoke(routing);
 
             endpointConfiguration.UsePersistence<LearningPersistence>();
-            
+
             var pipeline = endpointConfiguration.Pipeline;
             pipeline.Register(new SignalR_Incoming(), "Stores SignalR user identifier into context.");
             pipeline.Register(new SignalR_Outgoing(), "Propagates SignalR user identifier to outgoing messages.");
-            
+
+            ConfigurePlatform(endpointConfiguration);
 
             var conventions = endpointConfiguration.Conventions();
             conventions.DefiningCommandsAs(t => t.Namespace != null && t.Namespace.EndsWith("Commands"));
@@ -28,6 +30,38 @@ namespace Shared.Configuration
             endpointConfiguration.Recoverability().Delayed(s => s.NumberOfRetries(0));
 
             return endpointConfiguration;
+        }
+
+        static void ConfigurePlatform(EndpointConfiguration endpointConfiguration)
+        {
+            var servicePlatformConnection = ServicePlatformConnectionConfiguration.Parse(@"{
+                    ""Heartbeats"": {
+                        ""Enabled"": false,
+                        ""HeartbeatsQueue"": ""Particular.ServiceControl"",
+                        ""Frequency"": ""00:00:10"",
+                        ""TimeToLive"": ""00:00:40""
+                    },
+                    ""MessageAudit"": {
+                        ""Enabled"": true,
+                        ""AuditQueue"": ""audit""
+                    },
+                    ""CustomChecks"": {
+                        ""Enabled"": false,
+                        ""CustomChecksQueue"": ""Particular.ServiceControl""
+                    },
+                    ""ErrorQueue"": ""error"",
+                    ""SagaAudit"": {
+                        ""Enabled"": true,
+                        ""SagaAuditQueue"": ""audit""
+                    },
+                    ""Metrics"": {
+                        ""Enabled"": true,
+                        ""MetricsQueue"": ""Particular.Monitoring"",
+                        ""Interval"": ""00:00:01""
+                    }
+                }");
+
+            endpointConfiguration.ConnectToServicePlatform(servicePlatformConnection);
         }
     }
 }
