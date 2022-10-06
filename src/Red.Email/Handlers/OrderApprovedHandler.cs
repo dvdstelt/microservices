15602871d3f6ac34999b96749855ee0dc1e7d4e7
Dennis <dvdstelt@gmail.com>
2022-10-06 23:33:20 +0200

    Added sending email

---
+++ src/Red.Email/Handlers/OrderApprovedHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Mail;
+using NServiceBus;
+using NServiceBus.Logging;
+using Red.Data.Configuration;
+using Red.Data.Entities;
+using Yellow.Messages.Events;
+
+namespace Yellow.Ticketing.Handlers;
+
+public class OrderApprovedHandler : IHandleMessages<OrderApproved>
+{
+    static readonly ILog Log = LogManager.GetLogger<OrderApprovedHandler>();
+
+    readonly RedLiteDatabase db;
+    
+    public OrderApprovedHandler(RedLiteDatabase db) => this.db = db;
+    
+    public Task Handle(OrderApproved message, IMessageHandlerContext context)
+    {
+        var movie = db.Query<Movie>().Where(m => m.Identifier == message.MovieId).Single();
+        var theater = TheatersContext.GetTheaters().Single(t => t.Id == message.TheaterId);
+
+        var body =
+            $"Hi,\n\nYou ordered {message.NumberOfTickets} tickets for the movie '{movie.Title}' in theater {theater.Name} at {message.Time}.\n\nWe hope you enjoy your time at our theater.";
+        
+        var smtpClient = new SmtpClient("localhost")
+        {
+            Port = 25,
+            // Credentials = new NetworkCredential("username", "password"),
+            // EnableSsl = true,
+        };
+    
+        smtpClient.Send("info@compilesoftware.nl", "dennis@bloggingabout.net", $"Your ticket for {movie.Title}", body);
+        
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Red.Email/Program.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using NServiceBus;
+using Red.Data.Configuration;
+using Serilog;
+using Serilog.Events;
+using Serilog.Sinks.SystemConsole.Themes;
+using Shared.Configuration;
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+    .MinimumLevel.Override("NServiceBus", LogEventLevel.Warning)
+    .Enrich.FromLogContext()
+    .WriteTo.Console(
+        outputTemplate: "{Timestamp:HH:mm:ss} [{Level:u3}] ({SourceContext}) {Message}{NewLine}{Exception}",
+        theme: SystemConsoleTheme.Literate)
+    .CreateLogger();
+    
+var host = Host.CreateDefaultBuilder(args)
+    .UseSerilog()
+    .UseNServiceBus(context =>
+    {
+        var endpointConfiguration = new EndpointConfiguration("Red.Email").ApplyCommonConfiguration();
+        endpointConfiguration.UseSerialization<NewtonsoftJsonSerializer>();
+
+        endpointConfiguration.RegisterComponents(s =>
+        {
+            s.ConfigureComponent(() => new RedLiteDatabase(), DependencyLifecycle.InstancePerUnitOfWork);
+        });
+        
+        return endpointConfiguration;
+    })
+    .Build();
+
+var hostEnvironment = host.Services.GetRequiredService<IHostEnvironment>();
+Console.Title = hostEnvironment.ApplicationName;
+
+Log.Information("Press CTRL+C to quit the application...");
+
+host.Run();
+
+Log.Information("Application shut down...");
+Log.CloseAndFlush();
